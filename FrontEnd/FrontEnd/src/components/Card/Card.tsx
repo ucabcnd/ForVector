eefc5963a4752c9022b457ea66e3d1672c6f5aa4
ucabcnd <73539195+ucabcnd@users.noreply.github.com>
2021-12-02 14:18:57 +0000

    added sqlitedb as well as ATTEMPTED REORDERING ELEMENTS

--- FrontEnd/FrontEnd/src/components/Card/Card.tsx
+++ FrontEnd/FrontEnd/src/components/Card/Card.tsx
@@ -1,38 +1,34 @@
-import { IonCard, IonCardContent, IonCardHeader, IonCardTitle,IonAlert} from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonAlert, IonItem, IonReorder, IonIcon } from '@ionic/react';
 import React, { useState, useEffect } from "react";
 import './Card.css';
-import axios from "axios";
-import { waitFor } from '@testing-library/dom';
+import { square } from 'ionicons/icons';
 
-interface Data{
-    type: string,
-    title: string,
-    position:number
+interface Data {
+  type: string,
+  title: string,
+  position: number
 }
 
 interface ContainerProps {
-    data: Data;
-    index: number;
+  data: Data;
+  index: number;
 }
 
 
-function getgif(type: string):string{ 
-  
-  if (type.includes('bank-draft'))
-    {
-      return 'https://media.giphy.com/media/cPxRDvlSj9QKA/giphy.gif'
-    }
-    else if (type.includes('bill-of-lading'))
-    {
-      return 'https://media.giphy.com/media/l0ExdMHUDKteztyfe/giphy.gif'
-    }
-    else if (type.includes('invoice'))
-    {
-      return 'https://media.giphy.com/media/LHZyixOnHwDDy/giphy.gif'
-    }
-    else{
-      return '';
-    }
+function getgif(type: string): string {
+
+  if (type.includes('bank-draft')) {
+    return 'https://media.giphy.com/media/cPxRDvlSj9QKA/giphy.gif'
+  }
+  else if (type.includes('bill-of-lading')) {
+    return 'https://media.giphy.com/media/l0ExdMHUDKteztyfe/giphy.gif'
+  }
+  else if (type.includes('invoice')) {
+    return 'https://media.giphy.com/media/LHZyixOnHwDDy/giphy.gif'
+  }
+  else {
+    return '';
+  }
 }
 
 
@@ -40,37 +36,40 @@ const Card: React.FC<ContainerProps> = (props) => {
   const [loaded, setLoaded] = useState<boolean>(false);
   const [show, setShow] = useState<boolean>(false);
   const [showExpanded, setShowExpanded] = useState<boolean>(false);
-  const [gifURL,setGifURL] = useState<string>('');
+  const [gifURL, setGifURL] = useState<string>('');
 
-  function image_loaded(type:string){
+  function image_loaded(type: string) {
     setLoaded(true);
     setGifURL(getgif(type));
   }
 
   useEffect(
     () => {
-      const timeout = setTimeout(() => {setShow(true)}, 2000)
+      const timeout = setTimeout(() => { setShow(true) }, 2000)
       return () => clearTimeout(timeout)
     }, [show])
 
   return (
-          <IonCard className='Card' onClick={() => setShowExpanded(true)}>
-            <IonCardHeader>
-              <IonCardTitle>
-                {props.data.title}
-              </IonCardTitle>
-            </IonCardHeader>
-            <IonCardContent>
-              <img src={show&&loaded? getgif(props.data.type): '/thumbnails/loading.gif'} className='ImageGIF' onLoad={()=>image_loaded(props.data.type)}/>
-            </IonCardContent>
-            <IonAlert
-              isOpen={showExpanded}
-              onDidDismiss={() => setShowExpanded(false)}
-              cssClass='AlertClass'
-              header={props.data.title}
-              message={'<img src=' + gifURL + ' style=\'size:200%;\' />'}
-            />
-          </IonCard>
-  )};
+    <IonItem className="Item">
+    <IonCard className='Card' onClick={() => setShowExpanded(true)}>
+        <IonCardHeader>
+          <IonCardTitle>
+            {props.data.title + " " + props.data.position}
+          </IonCardTitle>
+        </IonCardHeader>
+        <IonCardContent>
+          <img src={show && loaded ? getgif(props.data.type) : '/thumbnails/loading.gif'} className='ImageGIF' onLoad={() => image_loaded(props.data.type)} />
+        </IonCardContent>
+        <IonAlert
+          isOpen={showExpanded}
+          onDidDismiss={() => setShowExpanded(false)}
+          cssClass='AlertClass'
+          header={props.data.title}
+          message={'<img src=' + gifURL + ' style=\'size:200%;\' />'}
+        />
+      </IonCard>
+    </IonItem>
+  )
+};
 
 export default Card;
--- FrontEnd/FrontEnd/src/pages/Home.tsx
+++ FrontEnd/FrontEnd/src/pages/Home.tsx
@@ -1,11 +1,34 @@
-import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonPage, IonReorderGroup, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
 import SampleData from '../sampleinput.json';
-import { Console } from 'console';
+import { ItemReorderEventDetail } from '@ionic/core';
+import {DragDropContext,Droppable,Draggable, DropResult} from 'react-beautiful-dnd';
+import { useState } from 'react';
 import Card from '../components/Card/Card';
 
+function doReorder(event: CustomEvent<ItemReorderEventDetail>) {
+  // The `from` and `to` properties contain the index of the item
+  // when the drag started and ended, respectively
+  console.log('Dragged from index', event.detail.from, 'to', event.detail.to);
+
+  // Finish the reorder and position the item in the DOM based on
+  // where the gesture ended. This method can also be called directly
+  // by the reorder group
+  event.detail.complete();
+}
+
 const Home: React.FC = () => {
+  const [cards,updateCards] = useState(SampleData);
+
+  function handleOnDragEnd(result: DropResult){
+    if (!result.destination) return;
+    console.log(result);
+    const items = Array.from(cards);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination!.index, 0,reorderedItem);
+    updateCards(items);
+  }
+
   return (
     <IonPage>
       <IonHeader>
@@ -14,11 +37,22 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonReorderGroup>
-          {SampleData.map((info, i) => (
-            <Card key={i} data={info} index={i} />
-          ))}
-        </IonReorderGroup>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="cards" direction='horizontal'>
+            {(provided)=>(
+              <ul {...provided.droppableProps} ref={provided.innerRef}>
+                {cards.map((info, i) => (
+                <Draggable key={info.title} draggableId={info.title} index={i}>
+                    {(provided)=>(
+                      <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} className="CardList"><Card data={info} index={i} /></li>
+                    )} 
+                </Draggable>
+                ))}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
       </IonContent>
     </IonPage>
   );
